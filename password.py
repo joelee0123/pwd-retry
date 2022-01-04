288adb6a01181ec0c663976a8ab31167e7b4b65a
JOELEE0123 <joelee650123@gmail.com>
2022-01-04 14:14:29 +0800

    add new file

---
+++ password.py
@@ -0,0 +1,11 @@
+password = 'a123456'
+i = 3 # 剩餘次數
+while i > 0:
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功')
+		break # 跳出迴圈
+	else:
+		print('密碼錯誤')
+		if i > 0
+			print('你還有', i,'機會')
